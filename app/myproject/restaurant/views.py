741ecd3dcc42c0def3f978905ce6e229fedf5d07
Brett Byrnes <brettbynes@gmail.com>
2023-02-05 09:03:13 -0500

    final project wk1

--- app/myproject/restaurant/views.py
+++ app/myproject/restaurant/views.py
@@ -10,11 +10,17 @@ from django.http import HttpResponse
 
 
 # Create your views here.
+def index(request):
+    return render(request, "index.html", {})
+
+
 def home(request):
     return render(request, 'index.html')
 
+
 def about(request):
     return render(request, 'about.html')
+
 
 def reservations(request):
     date = request.GET.get('date',datetime.today().date())
