72efb5c02b91c101faf608b76dfb0f612bc741ab
Brett Byrnes <brettbynes@gmail.com>
2023-02-18 03:15:02 +0000

    added auth isauthenticatedorreadonly

--- app/myproject/api/views.py
+++ app/myproject/api/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
 from rest_framework.response import Response
-from rest_framework.generics import ListCreateAPIView, ListAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, BasePermission
 
 from restaurant.models import MenuItem, Reservation
 
 from .serializers import MenuItemSerializer, ReservationSerializer
+
+
+SAFE_METHODS = ["GET"]
+class IsAuthenticatedOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if ((request.method in SAFE_METHODS) or (request.user.is_authenticated)):
+            return True
+        else:
+            return False
+
 
 @api_view()
 @permission_classes([IsAuthenticated])
@@ -14,17 +24,20 @@ def message(request):
     return Response({"message":"This view is protected"})
 
 
-class MenuItemView(ListAPIView):
+class MenuItemView(ListCreateAPIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
 
 
 class SingleMenuItemView(RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
     
     
 class ReservationView(ListCreateAPIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Reservation.objects.all()
     serializer_class = ReservationSerializer
 
